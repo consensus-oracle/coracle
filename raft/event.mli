87559ee4a73a0c9f8c3d5cc95f29712931fac961
heidi-ann <hh360@cam.ac.uk>
2015-07-07 14:45:01 +0100

    removing Event

--- raft/event.mli
+++
@@ -1,4 +0,0 @@
-open Common
-open Rpcs
-
-val eval: rpc Io.input -> State.t -> State.t option * rpc Io.output list
--- raft/raft.ml
+++ raft/raft.ml
@@ -4,7 +4,13 @@ let add_peers = State.add_nodes
 let state_to_string = State.to_string
 
 type msg = Rpcs.rpc
-let eval = Event.eval
 let msg_to_string = Rpcs.rpc_to_string
 let msg_serialize = Rpcs.rpc_serialize
 let msg_deserialize = Rpcs.rpc_deserialize
+
+open Io 
+let eval event state =
+	match event with
+	| PacketArrival (id,pkt) -> Election_io.receive_pkt id pkt state
+	| Startup _ -> Election_io.startup state
+  | Timeout timer -> Election_io.receive_timeout timer state
