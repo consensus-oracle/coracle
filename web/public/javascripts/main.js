8bc5f2cae66ffe233e609752c7d7cbbc21e306ee
hesthorp <hesthorp@gmail.com>
2015-07-16 19:39:55 +0100

    style results and errors

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
     $('#runSim').click(function () {
 		//clear all validationErrors and results
 		$('.validationError').remove();
-		$('#SimResults').html('');
+    $('.has-error').removeClass('has-error');
+		$('#resultsPanel').addClass('hidden');
 		if (validateSettings()){
 			var oldButtonText = $('#runSim').html();
 			$('#runSim').html('Running...');
@@ -19,13 +20,16 @@ $(document).ready(function () {
 			}, 
 			function(response){
 				var result;
+        //response.stdout ='{"packets dispatched":8,"packets received":8,"packets dropped":0}';
+        //response.error = null;
 				if (response.error != null){
 					console.log(response);
-					result = "Failed: " + response.stderr;
+					result = validationError("Failed: " + response.stderr);
 				}
 				else{
 					result = createTable(JSON.parse(response.stdout));
 				}
+        $('#resultsPanel').removeClass('hidden');
 				$('#SimResults').html(result);
 				$('#runSim').html(oldButtonText);
 				$('#runSim').removeAttr("disabled");
@@ -34,24 +38,30 @@ $(document).ready(function () {
     });
 	
 	function validationError(errorText){
-		return "<label class='validationError'><font color='red'>" + 
-			errorText +"</font></label>";
+		return '<div class="alert alert-danger validationError" role="alert">'
+      +'<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>'
+      +'<span class="sr-only">Error:</span>'
+  + errorText +
+  '</div>';
 	}
 	
 	function validateSettings(){
 		var valid = true;
 		if (!checkBounds(parseInt($('#numNodes').val()),2,maxNodes)){
 			$('#numNodes').after(validationError("Must be an integer between 2 and " + maxNodes));
+      $('#nodesDiv').addClass('has-error');
 			valid = false;
 		}
 		
 		if (!checkBounds(parseFloat($("#lossSlider").val()),0,1)){
 			$('#loss').after(validationError("Must be a number between 0 and 100"));
+      $('#lossDiv').addClass('has-error');
 			valid = false;
 		}
 		
 		if (!checkBounds(parseInt($('#termination').val()),1,maxTermination)){
-			$('#loss').after(validationError("Must be an integer between 1 and " + maxTermination));
+			$('#termination').after(validationError("Must be an integer between 1 and " + maxTermination));
+      $('#terminationDiv').addClass('has-error');
 			valid = false;
 		}
 		return valid;
@@ -63,13 +73,13 @@ $(document).ready(function () {
 	
 	function createTable(jsonObject){
 		//start of html for the table
-		var table = "<table id='results' border='1'>\
-			<tr>";
+		var table = "<table class='table table-bordered' id='results'>\
+			<tr><thead>";
 		//add in the headers from the JSON variables
 		for (var key in jsonObject){
 			table += "<th>" + key + "</th>";
 		}
-		table += "</tr><tr>";
+		table += "</tr></thead><tr>";
 		
 		//now add in the values, assumes no list values
 		for (var key in jsonObject){
