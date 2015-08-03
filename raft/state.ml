bf02bc89c3b71b87862f5ee405ba851a0ec7a94a
Heidi-ann <hh360@cam.ac.uk>
2015-08-03 14:15:59 +0100

    basic log infastructure

--- raft/state.ml
+++ raft/state.ml
@@ -41,11 +41,15 @@ type config = {
   client_timer: int option;
 }
 
+type entry = index * term * cmd
+type log = entry list
+
 type t = {
  term: term;
  mode: mode_state;
  last_index: index;
  last_term: term;
+ log: log;
  node_ids: id list;
  config: config;
 }
@@ -56,6 +60,7 @@ let init id config = {
  last_index = 0;
  last_term = 0;
  node_ids = create_nodes config.servers id 1;
+ log = [];
  config;
 }
 
@@ -64,6 +69,7 @@ let refresh t = {
   mode= follower;
   last_index = 0;
   last_term = 0;
+  log = t.log;
   node_ids=t.node_ids;
   config=t.config
 }
@@ -91,6 +97,13 @@ let mode_to_json = function
       ("mode type", `String "leader");
     ]
 
+let entry_to_json (index,term,cmd) = 
+  `Assoc [
+    ("index",`Int index);
+    ("term", `Int term);
+    ("cmd", `Int cmd);
+    ]
+
 let to_json s =
   `Assoc [
     ("term", `Int s.term);
@@ -98,5 +111,6 @@ let to_json s =
     ("last log index", `Int s.last_index);
     ("last log term", `Int s.last_term);
     ("peers", `List (List.map (fun i -> `Int i) s.node_ids));
+    ("log",`List (List.map entry_to_json s.log));
   ]
 
