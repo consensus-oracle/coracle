688c7080a5d69fcc9067cc67dbd4f89e13374598
Heidi-ann <hh360@cam.ac.uk>
2015-08-12 23:29:16 +0100

    minor cosmetic changes

--- web/public/javascripts/createGraphs.js
+++ web/public/javascripts/createGraphs.js
@@ -1,13 +1,13 @@
 function createGraph1(chartData){
-  var svg = d3.select('#graphs').append('svg').attr('width',500).attr('height',500);
+  var svg = d3.select('#graphs').append('svg').attr('width',1000).attr('height',300);
   var chart = nv.models.lineChart()
                 .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
                 .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
                 .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
                 .showYAxis(true)        //Show the y-axis
                 .showXAxis(true)        //Show the x-axis
-                .width(500)
-                .height(500)
+                .width(1000)
+                .height(300)
                 .xDomain([chartData['x axis'].start,chartData['x axis'].end])
                 .yDomain([chartData['y axis'].start,chartData['y axis'].end])
   ;
